456a0b47be3b0dd84f326d34b7deda3dba08b9e9
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2022-10-09 13:28:53 +0300

    Add looping to player

--- src/ContentPlayer.tsx
+++ src/ContentPlayer.tsx
@@ -1,11 +1,49 @@
+import React from "react";
+
 const testData = [
     'https://imgs.xkcd.com/comics/easy_or_hard.png',
     'https://imgs.xkcd.com/comics/archimedes_principle.png',
     'https://imgs.xkcd.com/comics/movie_ages.png',
 ]
 
-export const ContentPlayer = () => {
-    return <div className='content-player-container'>
-        <img className='content-image' src={testData[2]}></img>
-    </div>
+const INTERVAL = 7000;
+interface ContentPlayerState {
+    playlist: string[];
+    index: number;
+}
+
+export class ContentPlayer extends React.Component<{}, ContentPlayerState> {
+    private timerId?: NodeJS.Timer = undefined;
+
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            playlist: [...testData],
+            index: 0,
+        }
+    }
+
+    nextIndex() {
+        const newIndex = (this.state.index + 1) % this.state.playlist.length;
+        this.setState({
+            index: newIndex,
+        })
+    }
+    
+    componentDidMount(): void {
+        this.timerId = setInterval(this.nextIndex.bind(this), INTERVAL);
+    }
+    
+    componentWillUnmount(): void {
+        if (this.timerId) {
+            clearInterval(this.timerId);
+        }
+    }
+
+    render () {
+        const source = this.state.playlist[this.state.index];
+        return <div className='content-player-container'>
+            <img className='content-image' src={source}></img>
+        </div>
+    }
 }
